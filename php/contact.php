b3b6dc02431198e1073a475619b083a48e306bb4
Cody Gordon <codyjg10@gmail.com>
2022-01-07 02:34:02 -0500

    updated contact script

--- php/contact.php
+++ php/contact.php
@@ -1,10 +1,10 @@
 <?php
     $to = 'cody@codygordon.com';
-    $subject = $_POST['subject'];
+    $name = $_POST['name'];
     $message = $_POST['comments'];; 
     $from =  $_POST['email'];
     
-    $content = "From: $from\nSubject: $subject\nMessage: $message";
+    $content = "From: $from\nName: $name\nMessage: $message";
 
     if(mail($to, $subject, $content)){
         echo 'Your mail has been sent successfully.';
